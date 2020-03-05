b6238e14b9131c99b7b43839175831f5afe55de1
github username <github email address>
2020-03-05 12:58:16 -0800

    index page

--- src/components/BreachList.js
+++ src/components/BreachList.js
@@ -1,19 +1,16 @@
 import React, { Component} from 'react'
-// import BlogCard from './BreachCardv2'
-import BreachCard from './BreachCard'
-// import BreachCardv3 from './BreachCardv3'
-import { ThemeProvider, createMuiTheme, CssBaseline } from "@material-ui/core";
+import ProfileCard from './ProfileCard'
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 const baseTheme = createMuiTheme(); ///*** added ThemeProvider */
 
 class BreachList extends Component {
     renderResults = () => {
         return this.props.breaches.map(result => 
             <ThemeProvider theme={baseTheme}> 
-            <BreachCard key={result.id} result={result}/>
+            <ProfileCard key={result.id} result={result}/>
             </ThemeProvider>
         )
     }
-
 
     render(){
         return(
