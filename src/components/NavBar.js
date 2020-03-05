f07889acd0303f38af1b94433b9e4a2f756431f4
github username <github email address>
2020-03-05 12:59:34 -0800

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,20 +2,48 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import 'typeface-roboto';
-import {MenuIcon, Button, IconButton} from '@material-ui/core/'
-// import SvgIcon from '@material-ui/core/SvgIcon';
+import {Button, IconButton} from '@material-ui/core/'
 import {Link} from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import 'typeface-roboto';
+// import {NavLink} from 'react-router-dom'
+
+// const active = {
+//   'font-weight': 'bold'
+// }
+
+// const link = {
+//   width: '100px',
+//   padding: '12px',
+//   margin: '0 6px 6px',
+//   textDecoration: 'none',
+//   color: 'white'
+// }
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > svg': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
+}
 
 const NavBar = (props) => {
+  const classes = useStyles();
     return(
         <div>
-          <AppBar position="fixed">
+          <AppBar position="fixed" color="primary">
             <Toolbar>
               <IconButton edge="start" color="inherit" aria-label="menu">
-              {/* <HomeIcon /> */}
-
-                {/* <MenuIcon /> */}
+              <HomeIcon />
               </IconButton>
               <Typography variant="h6">
                 Hackornaut
@@ -23,7 +51,7 @@ const NavBar = (props) => {
               <Button color="inherit" component={Link} to="/">{props.loggedIn ? "Home" : "Login"}</Button>
               <Button color="inherit" component={Link} to="/about">Privacy</Button>
               <Button color="inherit" component={Link} to="/index">Index</Button>
-
+              {/* <NavLink to="/about" exact style={link} activeStyle={active} className={classes.root} >Testing</NavLink> */}
             </Toolbar>
           </AppBar>
         </div>
